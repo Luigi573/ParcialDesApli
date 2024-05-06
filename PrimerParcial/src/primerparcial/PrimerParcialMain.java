739be13e1cdddf4ea96528086f6d6ba2a7d22016
Luigi573 <106448901+Luigi573@users.noreply.github.com>
2024-05-06 13:11:14 -0600

    Add username greeting

--- PrimerParcial/src/primerparcial/PrimerParcialMain.java
+++ PrimerParcial/src/primerparcial/PrimerParcialMain.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class PrimerParcialMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String choice = "";
+        String choice = "", username;
         int table; 
+        
+        System.out.print("Ingrese su nombre de usuario: ");
+        username = scanner.nextLine();
         
         do{
         System.out.println("\nTablas de multiplicar");
         System.out.println("Seleccione una opcion.");
-        System.out.println("1. Mostrar tabla en específico");
+        System.out.println("1. Mostrar tabla en especifico");
         System.out.println("2. Mostrar tablas del 2 al 5");
         System.out.println("3. Salir");
         System.out.print("Opción: ");
@@ -21,6 +24,7 @@ public class PrimerParcialMain {
         
         switch(choice){
             case "1":
+                //System.out.println("Saludos, ["+ username + "]");
                 System.out.print("Ingrese el numero de la tabla a mostrar: ");
                 table = scanner.nextInt();
                 scanner.nextLine();
@@ -31,6 +35,9 @@ public class PrimerParcialMain {
                 }
                 break;
             case "2":
+                //System.out.println("Saludos, ["+ username + "]");
+                System.out.println("A continuacion mostrare las tablas de multiplicar del 2 al 5");
+                
                 for(int i = 2; i <= 5; i++){
                     System.out.println("Tabla del " + i);
                     for(int j = 1; j <= 10; j++){
@@ -40,7 +47,7 @@ public class PrimerParcialMain {
                 break;
             case "3":
                 System.out.println("Gracias por usar el generador de tablas de multiplicar...");
-                System.out.println("Que tenga un buen dia!");
+                System.out.println("Que tenga un buen dia! " );
                 break;
             default:
                 System.out.println("Opción no válida, inténtelo de nuevo...");
