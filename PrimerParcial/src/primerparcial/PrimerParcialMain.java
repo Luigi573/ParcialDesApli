673d2b3c55c5022dde92b9e07c170a75c19580b0
Luigi573 <106448901+Luigi573@users.noreply.github.com>
2024-05-06 12:52:48 -0600

    Add Custom table of times based on user input

--- PrimerParcial/src/primerparcial/PrimerParcialMain.java
+++ PrimerParcial/src/primerparcial/PrimerParcialMain.java
@@ -4,6 +4,41 @@ import java.util.Scanner;
 
 public class PrimerParcialMain {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice = "";
+        int table; 
         
+        do{
+        System.out.println("\nTablas de multiplicar");
+        System.out.println("Seleccione una opción.");
+        System.out.println("1. Mostrar tabla en específico");
+        System.out.println("2. Mostrar tablas del 2 al 5");
+        System.out.println("3. Salir");
+        System.out.print("Opción: ");
+        
+        choice = scanner.nextLine();
+        
+        switch(choice){
+            case "1":
+                System.out.println("///////////////////////////////////////////////////");
+                System.out.print("Ingrese el numero de la tabla a mostrar: ");
+                table = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Tabla del " + table);
+                
+                for(int i = 1; i <= 10; i++){
+                    System.out.println(table + " X " + i + " = " + (table * i));
+                }
+                break;
+            case "2":
+                
+                break;
+            case "3":
+                
+                break;
+            default:
+                System.out.println("Opción no válida, inténtelo de nuevo...");
+        }
+        }while(!choice.equals("3"));
     }
 }
